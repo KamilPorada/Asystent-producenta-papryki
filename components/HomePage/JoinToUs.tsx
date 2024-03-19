1d58bfc44ad3e14c1826fb799a251b9dedc9c555
Kamil0k <kamil.porada@onet.pl>
2024-03-19 19:21:58 +0100

    Small adjustements while deploying #2

--- components/HomePage/JoinToUs.tsx
+++ components/HomePage/JoinToUs.tsx
@@ -1,50 +1,53 @@
-import Button from "@components/UI/Button"
 import React, { useState, useEffect } from 'react'
 import { signIn, useSession, getProviders } from 'next-auth/react'
 import { LiteralUnion, ClientSafeProvider } from 'next-auth/react'
 import { BuiltInProviderType } from 'next-auth/providers'
 import logoGoogle from '../../public/assets/img/logo-google.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-	faUsers
-} from '@fortawesome/free-solid-svg-icons'
+import { faUsers } from '@fortawesome/free-solid-svg-icons'
+import Button from "@components/UI/Button"
 
 const JoinToUs = () => {
-	const { data: session } = useSession()
+    const { data: session } = useSession()
 
-	const [providers, setProviders] = useState<Record<
-		LiteralUnion<BuiltInProviderType, string>,
-		ClientSafeProvider
-	> | null>(null)
+    const [providers, setProviders] = useState<Record<
+        LiteralUnion<BuiltInProviderType, string>,
+        ClientSafeProvider
+    > | null>(null)
 
-	useEffect(() => {
-		;(async () => {
-			const res = await getProviders()
-			setProviders(res)
-		})()
-	}, [])
-	return (
-		<div className='h-72 flex flex-col justify-center items-center text-center bg-secondaryColor' >
-			<p className='mb-4 text-2xl md:text-3xl lg:text-4xl'>Nie masz jeszcze konta?</p>
-			<FontAwesomeIcon icon={faUsers} className='mb-2 text-4xl lg:text-5xl' />
-			<p className='mb-4 text-xl font-light'>Zapraszamy do nas za pośrednictwem konta Google!</p>
-			{providers &&
-				!session?.user?.name &&
-				Object.values(providers).map(provider => (
-					<div
-						className='flex flex-row justify-between mx-3 sm:text-lg rounded font-semibold bg-mainColor hover:scale-105 cursor-pointer transition-transform'
-						onClick={() => {
-							signIn(provider.id)
-						}}
-						key={provider.id}>
-						<div className='bg-gray-100 p-2 ring-mainColor rounded'>
-							<img alt='Google "G" Logo' src={logoGoogle.src} className='w-7 rounded-sm' />
-						</div>
-						<button className='mx-5'>Dołącz do nas</button>
-					</div>
-				))}
-		</div>
-	)
+    useEffect(() => {
+        (async () => {
+            const res = await getProviders()
+            setProviders(res)
+        })()
+    }, [])
+
+    const handleSignInClick = (providerId: string) => {
+        signIn(providerId)
+    }
+
+    return (
+        <div className='h-72 flex flex-col justify-center items-center text-center bg-secondaryColor' >
+            <p className='mb-4 text-2xl md:text-3xl lg:text-4xl'>Nie masz jeszcze konta?</p>
+            <FontAwesomeIcon icon={faUsers} className='mb-2 text-4xl lg:text-5xl' />
+            <p className='mb-4 text-xl font-light'>Zapraszamy do nas za pośrednictwem konta Google!</p>
+            {providers &&
+                !session?.user?.name &&
+                Object.values(providers).map(provider => (
+                    <div
+                        className='flex flex-row justify-between mx-3 sm:text-lg rounded font-semibold bg-mainColor hover:scale-105 cursor-pointer transition-transform'
+                        onClick={() => {
+                            handleSignInClick(provider.id)
+                        }}
+                        key={provider.id}>
+                        <div className='bg-gray-100 p-2 ring-mainColor rounded'>
+                            <img alt='Google "G" Logo' src={logoGoogle.src} className='w-7 rounded-sm' />
+                        </div>
+                        <Button onClick={() => handleSignInClick(provider.id)}>Dołącz do nas</Button>
+                    </div>
+                ))}
+        </div>
+    )
 }
 
 export default JoinToUs
--- components/Navigation/MainNavigation.tsx
+++ components/Navigation/MainNavigation.tsx
@@ -7,58 +7,64 @@ import { LiteralUnion, ClientSafeProvider } from 'next-auth/react'
 import { BuiltInProviderType } from 'next-auth/providers'
 
 function MainNavigation() {
-	const [isScrolled, setIsScrolled] = useState(false)
-	const { data: session } = useSession()
+    const [isScrolled, setIsScrolled] = useState(false)
+    const { data: session } = useSession()
 
-	const [providers, setProviders] = useState<Record<
-		LiteralUnion<BuiltInProviderType, string>,
-		ClientSafeProvider
-	> | null>(null)
+    const [providers, setProviders] = useState<Record<
+        LiteralUnion<BuiltInProviderType, string>,
+        ClientSafeProvider
+    > | null>(null)
 
-	useEffect(() => {
-		const handleScroll = () => {
-			const scrollPosition = window.scrollY
-			const headerHeight = document.getElementById('header')?.offsetHeight || 0
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY
+            const headerHeight = document.getElementById('header')?.offsetHeight || 0
 
-			setIsScrolled(scrollPosition > headerHeight)
-		}
+            setIsScrolled(scrollPosition > headerHeight)
+        }
 
-		window.addEventListener('scroll', handleScroll)
+        window.addEventListener('scroll', handleScroll)
 
-		return () => {
-			window.removeEventListener('scroll', handleScroll)
-		}
-	}, [])
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
 
-	useEffect(() => {
-		;(async () => {
-			const res = await getProviders()
-			setProviders(res)
-		})()
-	}, [])
-	return (
-		<nav
-			className={`flex flex-row justify-between md:justify-around items-center fixed top-0 left-0 w-screen h-20 transition-colors duration-300 z-10 ${
-				isScrolled ? 'bg-black bg-opacity-60' : ''
-			}`}>
-			<Brand />
-			{providers &&
-				!session?.user?.name &&
-				Object.values(providers).map(provider => (
-					<div
-						className='flex flex-row justify-between mx-3 sm:text-lg rounded font-semibold bg-mainColor hover:scale-105 cursor-pointer transition-transform'
-						onClick={() => {
-							signIn(provider.id)
-						}}
-						key={provider.id}>
-						<div className='bg-gray-100 p-2 ring-mainColor rounded'>
-							<img alt='Google "G" Logo' src={logoGoogle.src} className='w-7 rounded-sm' />
-						</div>
-						<button className='mx-5'>Zaloguj się</button>
-					</div>
-				))}
-		</nav>
-	)
+    useEffect(() => {
+        (async () => {
+            const res = await getProviders()
+            setProviders(res)
+        })()
+    }, [])
+
+    const handleSignInClick = (providerId: string, e: React.MouseEvent<HTMLButtonElement | HTMLDivElement, MouseEvent>) => {
+        e.preventDefault() 
+        signIn(providerId)
+    }
+
+    return (
+        <nav
+            className={`flex flex-row justify-between md:justify-around items-center fixed top-0 left-0 w-screen h-20 transition-colors duration-300 z-10 ${
+                isScrolled ? 'bg-black bg-opacity-60' : ''
+            }`}>
+            <Brand />
+            {providers &&
+                !session?.user?.name &&
+                Object.values(providers).map(provider => (
+                    <div
+                        className='flex flex-row justify-between mx-3 sm:text-lg rounded font-semibold bg-mainColor hover:scale-105 cursor-pointer transition-transform'
+                        onClick={(e) => {
+                            handleSignInClick(provider.id, e)
+                        }}
+                        key={provider.id}>
+                        <div className='bg-gray-100 p-2 ring-mainColor rounded'>
+                            <img alt='Google "G" Logo' src={logoGoogle.src} className='w-7 rounded-sm' />
+                        </div>
+                        <button className='mx-5'>Zaloguj się</button>
+                    </div>
+                ))}
+        </nav>
+    )
 }
 
 export default MainNavigation
